99ddbc315fb6b0640c32e7d5e98c5c8c6e86e9e1
eduardo <ecruzmatias5@gmail.com>
2025-03-25 17:31:03 -0500

    trasd

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 
 const env = {
-  PORT: process.env.SERVER_PORT || '3000',
+  PORT: process.env.SERVER_PORT || '8080',
   NAME: process.env.APP_NAME,
 }
 
